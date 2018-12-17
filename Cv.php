a82701682db350dc99dad2d1605cf0b5945b2fe1
evan <enourikyan@edenschool.fr>
2018-12-17 15:52:44 +0100

    ll

--- Cv.php
+++ Cv.php
@@ -16,7 +16,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link rel="stylesheet" href="./css/main.min.css">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
     <title>Cv template</title>
   </head>
   <body>
@@ -28,10 +28,10 @@
       <h1><?php echo $_prenom . '<span> ' . $_nom  .'</span>';?> </h1>
       <span><?php echo $_metier;?></span>
       <ul class="description">
-        <li><p><?php echo $_adresse;?></p></li>
-        <li><p><?php echo $_mail_adresse;?></p></li>
-        <li><p><?php echo $_numero;?></p></li>
-        <li><p><?php echo $_linkedin;?></p></li>
+        <li><i class="fas fa-map-marker-alt"></i><p><?php echo $_adresse;?></p></li>
+        <li><i class="fas fa-envelope"></i><p><?php echo $_mail_adresse;?></p></li>
+        <li><i class="fas fa-mobile-alt"></i><p><?php echo $_numero;?></p></li>
+        <li><i class="fab fa-linkedin"></i><p><?php echo $_linkedin;?></p></li>
       </ul>
     </div>
   </body>
