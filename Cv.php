130e057f031be424dbe71a67828feaddfe015001
evan <enourikyan@edenschool.fr>
2018-12-18 13:51:23 +0100

    e

--- Cv.php
+++ Cv.php
@@ -1,24 +1,19 @@
-<<<<<<< HEAD
 <?php
-  $_background = 'background.jpg';
-  $_rounded_image = 'pierre.jpg';
-  $_nom = 'Nom';
-  $_prenom = 'Prenom';
-  $_metier = 'Developpeur Web';
-  $_adresse = '3 place du bec';
-  $_mail_adresse = 'bec.kakic@free.fr';
-  $_numero = '067898765';
-  $_linkedin = 'Votre Linkedin';
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=Cv;charset=utf8', 'root', 'root', array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  ));
+}
+catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+}
+$reponse = $bdd->query('SELECT prenom, nom, metier, adresse, numero, adresse_mail, linkedin, background, your_image FROM user');
+$userdata = $reponse->fetch();
 ?>
-=======
-
->>>>>>> 75a27a5cb02ccfe60963509f36e524b81f695f86
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-<<<<<<< HEAD
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link rel="stylesheet" href="./css/main.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
@@ -26,28 +21,18 @@
   </head>
   <body>
     <div class="firstStrate"
-        style="background-image:url('./img/<?php echo $_background;?>')">
+        style="background-image:url('./uploads/<?php echo $userdata['background'];?>')">
       <div class="containerImage">
-        <img class="roundedImage" src="./img/<?php echo $_rounded_image;?>" alt="image qui montre la tête"/>
+        <img class="roundedImage" src="./uploads/<?php echo $userdata['your_image'];?>" alt="image qui montre la tête"/>
       </div>
-      <h1><?php echo $_prenom . '<span> ' . $_nom  .'</span>';?> </h1>
-      <span><?php echo $_metier;?></span>
+      <h1><?php echo $userdata['prenom'] . '<span> ' . $userdata['nom']  .'</span>';?> </h1>
+      <span><?php echo $userdata['metier'];?></span>
       <ul class="description">
-        <li><i class="fas fa-map-marker-alt"></i><p><?php echo $_adresse;?></p></li>
-        <li><i class="fas fa-envelope"></i><p><?php echo $_mail_adresse;?></p></li>
-        <li><i class="fas fa-mobile-alt"></i><p><?php echo $_numero;?></p></li>
-        <li><i class="fab fa-linkedin"></i><p><?php echo $_linkedin;?></p></li>
+        <li><i class="fas fa-map-marker-alt"></i><p><?php echo $userdata['adresse'];?></p></li>
+        <li><i class="fas fa-envelope"></i><p><?php echo $userdata['adresse_mail'];?></p></li>
+        <li><i class="fas fa-mobile-alt"></i><p><?php echo $userdata['numero'];?></p></li>
+        <li><i class="fab fa-linkedin"></i><p><?php echo $userdata['linkedin'];?></p></li>
       </ul>
-=======
-    <link rel="stylesheet" href="/css/cv.min.css">
-    <title>Cv template</title>
-  </head>
-  <body>
-    <div class="firstStrate">
-      <img class="roundedImage" src="./img/bilal.png" alt="image qui montre la tête"/>
-      <h1>Prenom <span>Nom</span></h1>
-      <p>Description (en 2 ligne)</p>
->>>>>>> 75a27a5cb02ccfe60963509f36e524b81f695f86
     </div>
   </body>
 </html>
---
+++ adminPage.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=Cv;charset=utf8', 'root', 'root', array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  ));
+}
+catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+}
+
+function uploadImage ($name) {
+    $link = null;
+
+    if (isset($_FILES[$name]) && $_FILES[$name]['error'] == 0) {
+            // Testons si le fichier n'est pas trop gros
+        if ($_FILES[$name]['size'] <= 1000000) {
+          $infosfichier = pathinfo($_FILES[$name]['name']);
+          $extension_upload = $infosfichier['extension'];
+          $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+          if (in_array($extension_upload, $extensions_autorisees)) {
+            $link = basename($_FILES[$name]['name']);
+            move_uploaded_file($_FILES[$name]['tmp_name'], 'uploads/' . $link);
+            echo "L'envoi a bien été effectué !";
+          }
+        }
+    }
+
+    return $link;
+}
+
+if (isset($_POST['submit'])) {
+  $backgroundLink = uploadImage('background');
+  $imageLink = uploadImage('your_image');
+
+  if (!empty($_POST['prenom']) && !empty($_POST['nom']) && !empty($_POST['metier']) && !empty($_POST['adresse']) && !empty($_POST['numero']) && !empty($_POST['adresse_mail']) && !empty($_POST['linkedin'])) {
+    // Insertion
+    $req = $bdd->prepare('INSERT INTO user(prenom, nom, metier, adresse, numero, adresse_mail, linkedin, background, your_image, id) VALUES(:prenom, :nom, :metier, :adresse, :numero, :adresse_mail, :linkedin, :background, :your_image, id)');
+    $req->execute(array(
+        'background' => $backgroundLink,
+        'your_image' => $imageLink,
+        'prenom' => $_POST['prenom'],
+        'nom' => $_POST['nom'],
+        'metier' => $_POST['metier'],
+        'adresse' => $_POST['adresse'],
+        'numero' => $_POST['numero'],
+        'adresse_mail' => $_POST['adresse_mail'],
+        'linkedin' => $_POST['linkedin']));
+
+    header("Location: Cv.php");
+  } else {
+    echo "Erreur lors de l'enregistrement de vos données";
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <form class="formAdmin" method="post" enctype="multipart/form-data">
+        <label>Background :</label><input type="file" name="background" value="" placeholder="monimage.extension (jpg, png)"/>
+        <label>Image Ronde :</label><input type="file" name="your_image" value="" placeholder="monimage.extension (jpg, png)"/>
+        <label>Prénom :</label><input type="text" name="prenom" value=""/>
+        <labeL>Nom :</label><input type="text" name="nom" value=""/>
+        <label>Addresse</label><input type="text" name="adresse" value="" placeholder="3 rue du bec"/>
+        <label>Adresse Mail</label><input type="Mail" name="adresse_mail" value="" placeholder="exemple@domaine.fr">
+        <label>Numéro :</label><input type="text" name="numero" value="Numéro" placeholder="ex:0605030203"/>
+        <label>Métier :</label><input type="text" name="metier" value=""/>
+        <label>Linkedin</label><input type="text" name="linkedin" value="" placeholder="linkedin"/>
+        <input type="submit" name="submit"/>
+      </form>
+  </body>
+</html>
+
+<?php
+
+ ?>
